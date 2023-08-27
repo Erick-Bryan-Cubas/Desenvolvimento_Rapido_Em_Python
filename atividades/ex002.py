5c2aeadc2cc4635827545c04225c3c698bdf62b9
Erick Bryan Cubas <thecubas@outlook.com>
2023-08-27 05:16:47 -0300

    commit

--- atividades/ex002.py
+++ atividades/ex002.py
@@ -10,10 +10,21 @@ def funcao_media ():
 print("Seja bem-vindo ao APROVA-ESTACIO")
 print("====================================")
 aluno = str(input("Aluno, informe seu nome completo: "))
-if (len(aluno) < 10):
+if (len(aluno)) < 10:
     aluno = str(input("Por gentileza, informe seu nome completo: "))
 calculo = funcao_media()
-print(calculo)
+
+if(calculo < 5):
+    print(f""" Média no curso de DESENVOLVIMENTO RÁPIDO EM PYTHON: {calculo}
+          {aluno.upper()}, infelizmente sua média não foi o suficiente para ser aprovado no curso. Tente novamente! """)
+elif(calculo <= 5 or calculo < 7):
+    print(f""" Média no curso de DESENVOLVIMENTO RÁPIDO EM PYTHON: {calculo}
+          {aluno.upper()}, sua média não foi o suficiente para ser aprovado. Contudo, está em recuperação. Boa sorte! """)
+else:
+    print(f""" Média no curso de DESENVOLVIMENTO RÁPIDO EM PYTHON: {calculo}
+          {aluno.upper()}, parabéns pela aprovação no curso! """)
+
+    
 
 
 
